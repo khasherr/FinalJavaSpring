2c29419e1af1a2aa32223325628f0c08845d2da6
leedy <leedy@192.168.2.22>
2020-04-17 04:23:50 -0400

    Update 3

---
+++ Final_Project/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into Research_Study (research_Title, research_Area, 
+research_Institution, research_Duration, posted_By, posted_Date, research_Detail, num_Participants) 
+values ('Research A', 'Area', 'Research Institution', '0 day', 'Researcher A', '2020-04-17', 'detail', 3);
